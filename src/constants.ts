5347c529d7e49f22402bc58a0f177ac3471bce05
Franklin Shera <fshera96@gmail.com>
2023-10-30 08:46:47 +0300

    add constants

---
+++ src/constants.ts
@@ -0,0 +1,7 @@
+export const STAMPED_STORE_HASH = process.env.STAMPED_STORE_HASH!;
+
+export const STAMPED_PUBLIC_API_KEY = process.env.STAMPED_PUBLIC_API_KEY!;
+
+export const STAMPED_PRIVATE_API_KEY = process.env.STAMPED_PRIVATE_API_KEY!;
+
+export const STAMPED_BASE_URL = "https://stamped.io/api";
